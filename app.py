70154eb0a3e95a6adc808b4d732d8130f33430bf
brian <b.kigame@yahoo.com>
2023-01-29 07:17:45 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, Response
+import time
+app = Flask(__name__, static_url_path="/Static")
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+
+if __name__ == "__main__":
+	app.run()
---
+++ backend/Static/js/dash.js
@@ -0,0 +1,36 @@
+const progress = document.querySelector('.hard-progress-done');
+progress.style.width = 50 + '%';
+progress.style.opacity = 1;
+
+const aprogress = document.querySelector('.average-progress-done');
+aprogress.style.width = 90 + '%';
+aprogress.style.opacity = 1;
+
+const easyprogress = document.querySelector('.easy-progress-done');
+easyprogress.style.width = 90 + '%';
+easyprogress.style.opacity = 1;
+
+
+const comprogress = document.querySelector('.complited-hard-progress-done');
+comprogress.style.width = 50 + '%';
+comprogress.style.opacity = 1;
+
+const compaprogress = document.querySelector('.complited-average-progress-done');
+compaprogress.style.width = 90 + '%';
+compaprogress.style.opacity = 1;
+
+const compeasyprogress = document.querySelector('.complited-easy-progress-done');
+compeasyprogress.style.width = 90 + '%';
+compeasyprogress.style.opacity = 1;
+
+const requiprogress = document.querySelector('.recruitment-que-done');
+requiprogress.style.width = 90 + '%';
+requiprogress.style.opacity = 1;
+
+const reactprogress = document.querySelector('.recruitment-activated-done');
+reactprogress.style.width = 50 + '%';
+reactprogress.style.opacity = 1;
+
+const reoptprogress = document.querySelector('.recruitment-opt-done');
+reoptprogress.style.width = 40 + '%';
+reoptprogress.style.opacity = 1;
